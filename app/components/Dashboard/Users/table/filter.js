259b2b4bfc7bbcc835c8837fe85dff974531d02c
Sudhanshu Saxena <sudhanshu.saxena@kitpay.in>
2018-08-09 17:11:54 +0530

    Added filter

--- app/components/Dashboard/Users/table/filter.js
+++ app/components/Dashboard/Users/table/filter.js
@@ -1,62 +1,70 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const Filter = ({ filterables, filter, onReportFilter}) =>{
-    let filterString = filter.filterString;
-    let filterOn = filter.filterOn;
-   
+class Filter extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.state = {
+            filterString: this.props.filter.filterString,
+            filterOn: this.props.filter.filterOn
+        };
+    }
+    
+    onTextChangeFilterValue(event) {
+        this.setState({filterString: event.target.value});
+    }
 
-    const onTextChangeFilterValue = (event) => {
-        filterString = event.target.value;
-    };
+    onChangeFilter(event) {
+        this.setState({filterOn: event.target.value});
+    }
 
-    const onChangeFilter = (event) => {
-        filterOn = event.target.value;
-    };
+    onApplyFilter() {
+        this.props.onReportFilter({
+            filterString: this.state.filterString,
+            filterOn: this.state.filterOn});
+    }
 
-    const onApplyFilter = () => {
-        onReportFilter({filterString, filterOn});
-    };
-
-    const onClearFilter = () => {
-        filterOn = '';
-        filterString = '';
-        onReportFilter({filterString, filterOn});
-    };
-
-    return (
+    onClearFilter() {
+        // Fix the clearing of selection
+        this.setState({filterOn: '', filterString: ''});
+        this.props.onReportFilter({filterString: '', filterOn: ''});
+    }
+    render() {
+        return (
         
-        <div className="form-group">
-            <label htmlFor="fieldSelect"> Select Field: </label>
+            <div className="form-group">
+                <label htmlFor="fieldSelect"> Select Field: </label>
 
-            <select className="form-control" style={{width: '50%'}}
-                id="fieldSelect" 
-                onChange={onChangeFilter}>
-                value={filter.filterOn}
-                <option value={''}>Select Filter</option>
-                { filterables
-                    .filter(e => e.filterable)
-                    .map( ( e, index) =>{
-                        return (<option value={e.field} key={'filter-' + e.field}>{e.label}</option>);
-                    })}     
-            </select>
-            <br/>
-            <label htmlFor="fieldVal"> Enter Filter Value: </label>
-            &nbsp;
-            <input type="text"
-               placeholder="Enter filter value"
-                id="fieldVal"
-                onChange={onTextChangeFilterValue}/>
-            <br/>
-            <br/>
-            <div className="btn-group btn-group-justified" style={{width: '30%'}}>
-                <a onClick={onApplyFilter} className="btn btn-default btn-sm">Apply Filter</a>
-                <a onClick={onClearFilter} className="btn btn-default  btn-sm">Clear Filter</a>  
+                <select className="form-control" style={{width: '50%'}}
+                    id="fieldSelect" 
+                    onChange={this.onChangeFilter.bind(this)}>
+                    value={this.state.filterOn}
+                    <option value={''}>Select Filter</option>
+                    { this.props.filterables
+                        .filter(e => e.filterable)
+                        .map( ( e, index) =>{
+                            return (<option value={e.field} key={'filter-' + e.field}>{e.label}</option>);
+                        })}     
+                </select>
+                <br/>
+                <label htmlFor="fieldVal"> Enter Filter Value: </label>
+                &nbsp;
+                <input type="text"
+                placeholder="Enter filter value"
+                    id="fieldVal"
+                    value={this.state.filterString}
+                    onChange={this.onTextChangeFilterValue.bind(this)}/>
+                <br/>
+                <br/>
+                <div className="btn-group btn-group-justified" style={{width: '30%'}}>
+                    <a onClick={this.onApplyFilter.bind(this)} className="btn btn-default btn-sm">Apply Filter</a>
+                    <a onClick={this.onClearFilter.bind(this)} className="btn btn-default  btn-sm">Clear Filter</a>  
+                </div>
+
             </div>
-
-        </div>
-    );
-};
+        );
+    }
+}
 
 Filter.propTypes = {
     filterables: PropTypes.array.isRequired,
